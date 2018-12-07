2689860d13652fec7a940def212a9d21e478d052
Žan V. Dragan <zan.v.dragan@progmbh.com>
2018-12-07 22:08:39 +0100

    Finish part 1 of day 3.

--- day-3/zvd/zvd-part-1.py
+++ day-3/zvd/zvd-part-1.py
@@ -1,45 +1,32 @@
-import re
+import re, collections
 
 
-in_file = open('input-1.txt', 'r')
+in_file = open('input.txt', 'r')
 out_file = open('output-1.txt', 'w')
 
-fabric = []
-multi_claims = []
+claims = {}
 
 for claim in in_file:
+	# Get claim's data.
     claim_text = re.search(r"^#(\d*) @ (\d*),(\d*): (\d*)x(\d*)$", claim)
-    claim_id = int(claim_text.group(1))
-    from_left = int(claim_text.group(2)) + 1
-    from_top = int(claim_text.group(3)) + 1
-    width = int(claim_text.group(4))
-    height = int(claim_text.group(5))
+    claim_id, from_left, from_top, width, height = map(int, claim_text.groups())
 
+	# Iterate over claim's coordinates.
     for y in range(from_top, from_top + height):
         for x in range(from_left, from_left + width):
             claim_coord = str(x) + '-' + str(y)
-            for curr_coord in fabric:
-                if curr_coord == claim_coord:
-                    for multi_claim in multi_claims:
-                        if multi_claim['left'] <= x <= multi_claim['left'] + multi_claim['width']:
-                            break
-                        elif x == multi_claim['left'] - 1:
-                            multi_claim['left'] = x
-                            multi_claim['width'] += 1
-                        elif x == multi_claim['left'] + multi_claim['width'] + 1:
-                            multi_claim['width'] += 1
-                    else:
-                        multi_claims.append({
-                            'left': x,
-                            'top': y,
-                            'width': 1,
-                            'height': 1
-                        })
-                    break
+            
+		    # Add coordinates to the claim dictionary.
+            if claim_coord not in claims:
+            	claims[claim_coord] = set([claim_id])
             else:
-                fabric.append(claim_coord)
+            	claims[claim_coord].add(claim_id)
+            
 
-print(fabric)
+overlaping = sum([len(claim) > 1 for claim in claims.values()])
+
+out_file.write(str(overlaping))
 
 in_file.close()
 out_file.close()
+
